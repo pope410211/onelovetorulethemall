6f5dc11ab03ad416735a5011bae7e4c414fff28e
Nick Pope <pope410211@gmail.com>
2015-09-26 11:38:34 -0400

    updating

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,10 @@
 (function(){
-"use strict";
+
 
 $('.dropdown-toggle').click(function(){
   console.log("Click me one more time");
-  $(this).parent().children('ul').toggleClass('d-none dropdown-menu');
   $(this).toggleClass('icon open');
-
+  $(this).parent().children('ul').toggleClass('d-none dropdown-menu');
 });
 
 
